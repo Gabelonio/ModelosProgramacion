ede5cdf0a01f7faf8e95ce0cd69165cc425bf9dd
Gabelonio <gabelonio@hotmail.com>
2019-01-14 21:46:17 -0500

    Diagrama UML añadido

--- Java/Patrones Estructurales/PatronFlyweight/src/puntosinformacion/GeneradorPtoInformacion.java
+++ Java/Patrones Estructurales/PatronFlyweight/src/puntosinformacion/GeneradorPtoInformacion.java
@@ -9,7 +9,7 @@ public class GeneradorPtoInformacion {
 		puntosInformacion=new ArrayList<PuntoInformacion>();
 	}
 	
-	public PuntoInformacion llamarPuntoInformacion(String identificador) {
+	public PuntoInformacion crearPuntoInformacion(String identificador) {
 		
 		for(int i=0;i<puntosInformacion.size();i++) {
 			if(puntosInformacion.get(i).getIdentificador().equals(identificador)) {
--- Java/Patrones Estructurales/PatronFlyweight/src/puntosinformacion/PruebaFlyWeight.java
+++ Java/Patrones Estructurales/PatronFlyweight/src/puntosinformacion/PruebaFlyWeight.java
@@ -4,9 +4,9 @@ public class PruebaFlyWeight {
 	public static void main(String args[]) {
 		GeneradorPtoInformacion generador=new GeneradorPtoInformacion();
 		
-		PuntoInformacion puntoUno=generador.llamarPuntoInformacion("PuntoAzul");
-		PuntoInformacion puntoDos=generador.llamarPuntoInformacion("PuntoVerde");
-		PuntoInformacion puntoTres=generador.llamarPuntoInformacion("PuntoAzul");
+		PuntoInformacion puntoUno=generador.crearPuntoInformacion("PuntoAzul");
+		PuntoInformacion puntoDos=generador.crearPuntoInformacion("PuntoVerde");
+		PuntoInformacion puntoTres=generador.crearPuntoInformacion("PuntoAzul");
 		
 		puntoUno.informar("Tienda de ropa");
 		puntoDos.informar("Tienda de cosmeticos");
